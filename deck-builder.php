d45be4aa723ea2cc5d6effe520c6de84c19722e5
Steven Wang <slw3bm@virginia.edu>
2018-04-30 23:52:07 -0400

    Add primitive deck name checking using AngularJS

--- deck-builder.php
+++ deck-builder.php
@@ -10,10 +10,10 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Richard Li, Steven Wang">
     <link rel="stylesheet" type="text/css" href="css/main.css">
-
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.16/angular.min.js"></script>
 </head>
 
-<body>
+<body ng-app="deckApp">
 	<?php
 		if (!isset($_SESSION['user'])) {
 			header('Location: login.php');
@@ -26,7 +26,7 @@ session_start();
         <li style="float:right"><a href="tracker.php">Statistics</a></li>
         <li style="float:right"><a href="#" class="active">Decks</a></li>
     </ul>
-    <div class="container">
+    <div class="container" ng-controller="deckController">
     <h2>Deck Builder</h2>
         <div class="navbar vertical-menu" id = "deckmenu">
             <a href= "#d1" id = "deck1" class="active" onclick = "click1()">Deck 1</a>
@@ -36,11 +36,13 @@ session_start();
         </div>
         <div class="content">
 			<div class = "deckpadding">
-				<img class = "deckpng" alt="Pack" src="images/deck.png" style="height: 300px;" onclick="pop()">
+				<img class = "deckpng" alt="Pack" src="images/deck.png" style="height: 250px;" onclick="pop()">
 				<div class = "popupDeckName">
 					<form class = "popuptext" id = "myPopup">
-						Enter Deck Name: <input type = "text">
-						<button type="submit" onclick = "closepop()">Create Deck</button>
+						<p>Enter Deck Name: <input type="text" name="newDeckName"
+												ng-model="newDeckName" ng-keyup="checkName()"/></p>
+						<p>{{message}}</p>
+						<button type="submit" onclick="closepop()">Create Deck</button>
 					</form>
 				</div>
 			</div>
@@ -78,5 +80,29 @@ session_start();
 			</script>
         </div>
     </div>
+    
+    <script>
+		var deckApp = angular.module('deckApp', []);
+	
+		deckApp.controller("deckController", function ($scope, $http) 
+		{
+			$scope.message = "";
+
+			$scope.checkName = function() {
+// 				In the future this function can check the name against all existing decks in the XML
+				var newDeckName = $scope.newDeckName;
+				if (newDeckName == "")
+					$scope.message = "";
+				else if (newDeckName == "Deck 1" || newDeckName == "Deck 2" || newDeckName == "Deck 3")
+					$scope.message = "A deck with that name already exists!";
+				else
+					$scope.message = "Name is OK.";
+			}  
+
+// 			We can add another function here to actually submit the deck to our XML
+			
+		});
+    </script>
+    
 </body>
 </html>
